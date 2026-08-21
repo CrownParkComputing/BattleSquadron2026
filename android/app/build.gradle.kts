4080c8e76b56f9159a9635419b335dd4ad5c6810
CrownParkComputing <jonnyb666@hotmail.co.uk>
2026-08-21 11:32:44 +0100

    Add Battle Squadron Android launcher artwork

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 2
-        versionName = "0.2-native"
+        versionName = "0.2"
 
         ndk {
             abiFilters += "arm64-v8a"
